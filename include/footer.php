621b409500fbbc55c2e8a116b7f88896f4d7ff25
Mohammad Taha Qureshi <taha@g.c>
2025-08-05 04:07:15 +0500

    first commit

---
+++ include/footer.php
@@ -0,0 +1,78 @@
+<!-- FOOTER -->
+<footer class="footer">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-md-3">
+                <div class="footer-content">
+                    <a href="index.html" class="footer-content__logo">
+                        <img src="images/logo.png" class="img-fluid" alt="image">
+                    </a>
+                    <p>
+                        Aston Energy is committed to a cleaner and greener future by delivering efficient and affordable
+                        solar energy solutions. With a strong portfolio of completed projects and a reputation for
+                        excellence, we are Pakistan's trusted name in solar innovation.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-content">
+                    <h2>Quick links</h2>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Gallery</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Careers</a></li>
+                        <li><a href="">Countries</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-content">
+                    <h2>Our Products</h2>
+                    <ul>
+                        <li><a href="">Inverters</a></li>
+                        <li><a href="">Variable Frequency Drives</a></li>
+                        <li><a href="">UPS</a></li>
+                        <li><a href="">Energy Storage System</a></li>
+                        <li><a href="">Batteries</a></li>
+                        <li><a href="">Solar panels</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-content">
+                    <h2>Quick Contact</h2>
+                    <ul>
+                        <li><a href="tel:(042) 111 977 977">(042) 111 977 977</a></li>
+                        <li><a href="tel:+971 48839373">+971 48839373</a></li>
+                        <li><a href="mailto:info@crownmicroglobal.com">info@crownmicroglobal.com</a></li>
+                        <li>
+                            <a href="">
+                                Plot No S60806 Jafza South, Jebel Ali, Free Zone Dubai, United Arab Emirates
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyRight">
+        <p>© 2025 Aston Energy. All Rights Reserved | Powered by ZEROPIXEL</p>
+    </div>
+</footer>
+<!-- searchBtn -->
+<div class="searchPopup" id="searchBtn">
+    <button class="close" type="button">×</button>
+    <form>
+        <input placeholder="SEARCH" type="search" value="">
+        <div class="srch-btn">
+            <a href="#" class="themeBtn">Search</a>
+        </div>
+    </form>
+</div>
+
+<?php include 'include/js.php'; ?>
+
+</body>
+
+</html>
